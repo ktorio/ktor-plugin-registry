0527ff0460cfed14f1c7dce28f7241371bec3bf3
Bruce Hamilton <bruce.hamilton@jetbrains.com>
2024-01-19 11:40:30 +0100

    Remove shell script

--- publish_registry.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/zsh
-SPACE_PACKAGES_PUBLISH_TOKEN=$(cat space_token)
-curl -i \
-  -H "Authorization: Bearer $SPACE_PACKAGES_PUBLISH_TOKEN" \
-  https://packages.jetbrains.team/files/p/ktor/files/plugin-registry/ \
-  --upload-file build/distributions/registry.tar.gz
