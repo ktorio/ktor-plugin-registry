c45948a25813fbe93188889fa10b12581961d129
Violetta Goncharova <40871415+VGoncharova@users.noreply.github.com>
2024-03-15 11:01:40 +0200

    added new manifest format for mongodb plugin, mongo plugin moved to a new group

---
+++ plugins/server/org.mongodb/mongodb/2.2/databases.kt
@@ -0,0 +1,82 @@
+import com.mongodb.client.*
+import io.ktor.http.*
+import io.ktor.server.application.*
+import io.ktor.server.config.*
+import io.ktor.server.request.*
+import io.ktor.server.response.*
+import io.ktor.server.routing.*
+
+
+fun Application.configureDatabases() {
+    val mongoDatabase = connectToMongoDB()
+    val carService = CarService(mongoDatabase)
+    routing {
+        // Create car
+        post("/cars") {
+            val car = call.receive<Car>()
+            val id = carService.create(car)
+            println("id : $id")
+            call.respond(HttpStatusCode.Created, id)
+        }
+        // Read car
+        get("/cars/{id}") {
+            val id = call.parameters["id"] ?: throw IllegalArgumentException("No ID found")
+            carService.read(id)?.let { car ->
+                call.respond(car)
+            } ?: call.respond(HttpStatusCode.NotFound)
+        }
+        // Update car
+        put("/cars/{id}") {
+            val id = call.parameters["id"] ?: throw IllegalArgumentException("No ID found")
+            val car = call.receive<Car>()
+            carService.update(id, car)?.let {
+                call.respond(HttpStatusCode.OK)
+            } ?: call.respond(HttpStatusCode.NotFound)
+        }
+        // Delete car
+        delete("/cars/{id}") {
+            val id = call.parameters["id"] ?: throw IllegalArgumentException("No ID found")
+            carService.delete(id)?.let {
+                call.respond(HttpStatusCode.OK)
+            } ?: call.respond(HttpStatusCode.NotFound)
+        }
+    }
+}
+
+/**
+ * Establishes connection with a MongoDB database.
+ *
+ * The following configuration properties (in application.yaml/application.conf) can be specified:
+ * * `db.mongo.user` username for your database
+ * * `db.mongo.password` password for the user
+ * * `db.mongo.host` host that will be used for the database connection
+ * * `db.mongo.port` port that will be used for the database connection
+ * * `db.mongo.maxPoolSize` maximum number of connections to a MongoDB server
+ * * `db.mongo.database.name` name of the database
+ *
+ * IMPORTANT NOTE: in order to make MongoDB connection working, you have to start a MongoDB server first.
+ * See the instructions here: https://www.mongodb.com/docs/manual/administration/install-community/
+ * all the paramaters above
+ *
+ * @returns [MongoDatabase] instance
+ * */
+fun Application.connectToMongoDB(): MongoDatabase {
+    val user = environment.config.tryGetString("db.mongo.user")
+    val password = environment.config.tryGetString("db.mongo.password")
+    val host = environment.config.tryGetString("db.mongo.host") ?: "127.0.0.1"
+    val port = environment.config.tryGetString("db.mongo.port") ?: "27017"
+    val maxPoolSize = environment.config.tryGetString("db.mongo.maxPoolSize")?.toInt() ?: 20
+    val databaseName = environment.config.tryGetString("db.mongo.database.name") ?: "myDatabase"
+
+    val credentials = user?.let { userVal -> password?.let { passwordVal -> "$userVal:$passwordVal@" } }.orEmpty()
+    val uri = "mongodb://$credentials$host:$port/?maxPoolSize=$maxPoolSize&w=majority"
+
+    val mongoClient = MongoClients.create(uri)
+    val database = mongoClient.getDatabase(databaseName)
+
+    environment.monitor.subscribe(ApplicationStopped) {
+        mongoClient.close()
+    }
+
+    return database
+}
