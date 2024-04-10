e6d02e002993e057c0a961d68899e26fd61f5220
Sergey Antonov <AntonovSergeyS@gmail.com>
2024-04-10 16:46:31 +0200

    Fix install and routing
    (cherry picked from commit f39dde9ba254b2c566c75d1f2ddc2209493a8c3e)

--- plugins/server/com.ucasoft/simple-memory-cache/2.0/install.kt
+++ plugins/server/com.ucasoft/simple-memory-cache/2.0/install.kt
@@ -1,6 +1,7 @@
 import io.ktor.server.application.*
 import com.ucasoft.ktor.simpleCache.SimpleCache
 import com.ucasoft.ktor.simpleMemoryCache.*
+import kotlin.time.Duration.Companion.seconds
 
 public fun Application.configureCache() {
     install(SimpleCache) {
--- plugins/server/com.ucasoft/simple-redis-cache/2.0/routing.kt
+++ plugins/server/com.ucasoft/simple-redis-cache/2.0/routing.kt
@@ -1,17 +1,19 @@
+import com.ucasoft.ktor.simpleCache.cacheOutput
+import io.ktor.server.application.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
+import kotlin.random.Random
+import kotlin.time.Duration.Companion.seconds
 
-public fun Routing.cachedEndpoints() {
-    routing {
-        cacheOutput(2.seconds) {
-            get("/short") {
-                call.respond(Random.nextInt())
-            }
+public fun Route.cachedEndpoints() {
+    cacheOutput(2.seconds) {
+        get("/short") {
+            call.respond(Random.nextInt())
         }
-        cacheOutput {
-            get("/default") {
-                call.respond(Random.nextInt())
-            }
+    }
+    cacheOutput {
+        get("/default") {
+            call.respond(Random.nextInt())
         }
     }
 }
