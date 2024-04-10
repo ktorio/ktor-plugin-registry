6df585472f621666554d2f36556a166ee5302340
Sergey Antonov <AntonovSergeyS@gmail.com>
2024-04-10 16:46:31 +0200

    Fix install and routing
    (cherry picked from commit f39dde9ba254b2c566c75d1f2ddc2209493a8c3e)

--- plugins/server/com.ucasoft/simple-redis-cache/2.0/install.kt
+++ plugins/server/com.ucasoft/simple-redis-cache/2.0/install.kt
@@ -1,13 +1,14 @@
 import io.ktor.server.application.*
 import com.ucasoft.ktor.simpleCache.SimpleCache
 import com.ucasoft.ktor.simpleRedisCache.*
+import kotlin.time.Duration.Companion.seconds
 
 public fun Application.configureCache() {
     install(SimpleCache) {
         redisCache {
             invalidateAt = 10.seconds
-            host = config.redis.host
-            port = config.redis.firstMappedPort
+            host = "localhost"
+            port = 6379
         }
     }
 }
