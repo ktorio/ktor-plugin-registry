a51d4a4d37cdcb574a273bb3c2fe09be17fc64e8
Ido Flax <idoflax@gmail.com>
2024-04-18 13:52:47 +0200

    feat(ktor-server-kafka): - Ktor server kafka plugin, as shown in https://www.youtube.com/live/HAARGzM0ICs?si=QK-JdrzDPn3NG9Ff

---
+++ plugins/server/io.github.flaxoos/ktor-server-kafka-jvm/2.0/install.kt
@@ -0,0 +1,54 @@
+import io.ktor.server.application.*
+import io.github.flaxoos.ktor.server.plugins.kafka.Kafka
+import io.github.flaxoos.ktor.server.plugins.kafka.MessageTimestampType
+import io.github.flaxoos.ktor.server.plugins.kafka.TopicName
+import io.github.flaxoos.ktor.server.plugins.kafka.admin
+import io.github.flaxoos.ktor.server.plugins.kafka.common
+import io.github.flaxoos.ktor.server.plugins.kafka.consumer
+import io.github.flaxoos.ktor.server.plugins.kafka.consumerConfig
+import io.github.flaxoos.ktor.server.plugins.kafka.consumerRecordHandler
+import io.github.flaxoos.ktor.server.plugins.kafka.producer
+import io.github.flaxoos.ktor.server.plugins.kafka.registerSchemas
+import io.github.flaxoos.ktor.server.plugins.kafka.topic
+import io.ktor.client.HttpClient
+import io.ktor.server.application.Application
+import io.ktor.server.application.install
+
+// The contents of the `install` function will be used for the project template
+public fun Application.install() {
+    install(Kafka) {
+        schemaRegistryUrl = "my.schemaRegistryUrl"
+        val myTopic = TopicName.named("my-topic")
+        topic(myTopic) {
+            partitions = 1
+            replicas = 1
+            configs {
+                messageTimestampType = MessageTimestampType.CreateTime
+            }
+        }
+        common { // <-- Define common properties
+            bootstrapServers = listOf("my-kafka")
+            retries = 1
+            clientId = "my-client-id"
+        }
+        admin { } // <-- Creates an admin client
+        producer { // <-- Creates a producer
+            clientId = "my-client-id"
+        }
+        consumer { // <-- Creates a consumer
+            groupId = "my-group-id"
+            clientId = "my-client-id-override" //<-- Override common properties
+        }
+        consumerConfig {
+            consumerRecordHandler(myTopic) { record ->
+                // Do something with record
+            }
+        }
+        registerSchemas {
+            using { // <-- optionally provide a client, by default CIO is used
+                HttpClient()
+            }
+            // MyRecord::class at myTopic // <-- Will register schema upon startup
+        }
+    }
+}
