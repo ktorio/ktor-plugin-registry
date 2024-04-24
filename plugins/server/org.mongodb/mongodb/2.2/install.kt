a3c9ab5b28abb6e068cc35e7ce2e96686dc4cdbd
Violetta Goncharova <40871415+VGoncharova@users.noreply.github.com>
2024-04-24 19:22:48 +0300

    missing import

--- plugins/server/org.mongodb/mongodb/2.2/install.kt
+++ plugins/server/org.mongodb/mongodb/2.2/install.kt
@@ -2,6 +2,7 @@ import io.ktor.http.*
 import io.ktor.server.application.*
 import io.ktor.server.request.*
 import io.ktor.server.response.*
+import io.ktor.server.routing.*
 import java.sql.*
 import kotlinx.coroutines.*
 
