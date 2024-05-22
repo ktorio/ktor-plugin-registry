4b02c5aba157fb16c098ac804e5bdf0a166a5ba2
Bruce Hamilton <bruce.hamilton@jetbrains.com>
2024-05-22 10:17:50 +0200

    KTOR-7018 Fix DuplicatePluginException on ApplicationTest

--- plugins/server/io.ktor/routing/2.0/test.kt
+++ plugins/server/io.ktor/routing/2.0/test.kt
@@ -7,9 +7,6 @@ import kotlin.test.*
 class ApplicationTest {
     @Test
     fun testRoot() = testApplication {
-        application {
-            configureRouting()
-        }
         client.get("/").apply {
             assertEquals(HttpStatusCode.OK, status)
             assertEquals("Hello World!", bodyAsText())
