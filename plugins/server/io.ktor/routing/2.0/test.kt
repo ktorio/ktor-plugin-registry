40a28e3c7a0856066c25a19c89896c2f6d6002e0
Bruce Hamilton <bruce.hamilton@jetbrains.com>
2024-05-28 10:20:47 +0200

    Revert "KTOR-7018 Fix DuplicatePluginException on ApplicationTest"
    This reverts commit 4b02c5aba157fb16c098ac804e5bdf0a166a5ba2.

--- plugins/server/io.ktor/routing/2.0/test.kt
+++ plugins/server/io.ktor/routing/2.0/test.kt
@@ -7,6 +7,9 @@ import kotlin.test.*
 class ApplicationTest {
     @Test
     fun testRoot() = testApplication {
+        application {
+            configureRouting()
+        }
         client.get("/").apply {
             assertEquals(HttpStatusCode.OK, status)
             assertEquals("Hello World!", bodyAsText())
