2b77b28d7827722c785a88b9ed11f8ea81f73a32
Osip Fatkullin <osip.fatkullin@gmail.com>
2024-07-10 13:15:58 +0300

    Update H2 and Exposed versions

--- plugins/server/org.jetbrains/exposed/2.2/UsersSchema.kt
+++ plugins/server/org.jetbrains/exposed/2.2/UsersSchema.kt
@@ -32,7 +32,8 @@ class UserService(database: Database) {
 
     suspend fun read(id: Int): ExposedUser? {
         return dbQuery {
-            Users.select { Users.id eq id }
+            Users.selectAll()
+                .where { Users.id eq id }
                 .map { ExposedUser(it[Users.name], it[Users.age]) }
                 .singleOrNull()
         }
