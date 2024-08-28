ee91ef3c25d55404f6c4f441c3bc8a9a5c844a3c
Bruce Hamilton <bruce.hamilton@jetbrains.com>
2024-08-28 12:43:50 +0200

    #108 Remove bad defaults for Compression plugin

--- plugins/server/io.ktor/compression/2.0/install.kt
+++ plugins/server/io.ktor/compression/2.0/install.kt
@@ -3,13 +3,5 @@ import io.ktor.server.plugins.compression.*
 import io.ktor.server.response.*
 
 public fun Application.configureHTTP() {
-    install(Compression) {
-        gzip {
-            priority = 1.0
-        }
-        deflate {
-            priority = 10.0
-            minimumSize(1024) // condition
-        }
-    }
+    install(Compression)
 }
