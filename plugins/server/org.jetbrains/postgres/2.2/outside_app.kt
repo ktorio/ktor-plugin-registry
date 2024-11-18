0db2f57f252234c93747422b4dee02713f02a2ed
Bruce Hamilton <bruce.hamilton@jetbrains.com>
2024-11-18 10:51:17 +0100

    KTOR-5515 Improvements for Postgres plugin

--- plugins/server/org.jetbrains/postgres/2.2/outside_app.kt
+++ plugins/server/org.jetbrains/postgres/2.2/outside_app.kt
@@ -1,9 +1,6 @@
-import io.ktor.http.*
 import io.ktor.server.application.*
-import io.ktor.server.request.*
-import io.ktor.server.response.*
-import java.sql.*
-import kotlinx.coroutines.*
+import java.sql.Connection
+import java.sql.DriverManager
 
 /**
  * Makes a connection to a Postgres database.
@@ -29,9 +26,11 @@ import kotlinx.coroutines.*
 fun Application.connectToPostgres(embedded: Boolean): Connection {
     Class.forName("org.postgresql.Driver")
     if (embedded) {
+        log.info("Using embedded H2 database for testing; replace this flag to use postgres")
         return DriverManager.getConnection("jdbc:h2:mem:test;DB_CLOSE_DELAY=-1", "root", "")
     } else {
         val url = environment.config.property("postgres.url").getString()
+        log.info("Connecting to postgres database at $url")
         val user = environment.config.property("postgres.user").getString()
         val password = environment.config.property("postgres.password").getString()
 
