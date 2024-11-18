666895b9b8998da620833c436bf8aa8f26d4d25e
Bruce Hamilton <bruce.hamilton@jetbrains.com>
2024-11-18 10:51:17 +0100

    KTOR-5515 Improvements for Postgres plugin

--- plugins/server/org.jetbrains/postgres/2.2/install.kt
+++ plugins/server/org.jetbrains/postgres/2.2/install.kt
@@ -2,10 +2,8 @@ import io.ktor.http.*
 import io.ktor.server.application.*
 import io.ktor.server.request.*
 import io.ktor.server.response.*
-import io.ktor.server.application.*
 import io.ktor.server.routing.*
-import java.sql.*
-import kotlinx.coroutines.*
+import java.sql.Connection
 
 public fun Application.configureDatabases() {
     val dbConnection: Connection = connectToPostgres(embedded = true)
