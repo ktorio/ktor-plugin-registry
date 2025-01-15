69fc1cf72e8834c752414f4400818969350077d5
DamirDenis-Tudor <denis-tudor.damir@student.tuiasi.ro>
2025-01-15 12:27:36 +0200

    Plugin updated to newest version.

--- plugins/server/io.github.damirdenis-tudor/ktor-server-rabbitmq/3.0/install.kt
+++ plugins/server/io.github.damirdenis-tudor/ktor-server-rabbitmq/3.0/install.kt
@@ -1,44 +1,73 @@
-import com.rabbitmq.client.BuiltinExchangeType
-import io.github.damir.denis.tudor.ktor.server.rabbitmq.KabbitMQ
-import io.github.damir.denis.tudor.ktor.server.rabbitmq.dsl.extensions.basicAck
-import io.github.damir.denis.tudor.ktor.server.rabbitmq.dsl.extensions.exchangeDeclare
-import io.github.damir.denis.tudor.ktor.server.rabbitmq.dsl.extensions.queueBind
-import io.github.damir.denis.tudor.ktor.server.rabbitmq.dsl.extensions.queueDeclare
+import io.github.damir.denis.tudor.ktor.server.rabbitmq.dsl.basicConsume
+import io.github.damir.denis.tudor.ktor.server.rabbitmq.dsl.basicPublish
+import io.github.damir.denis.tudor.ktor.server.rabbitmq.dsl.exchangeDeclare
+import io.github.damir.denis.tudor.ktor.server.rabbitmq.dsl.queueBind
+import io.github.damir.denis.tudor.ktor.server.rabbitmq.dsl.queueDeclare
+import io.github.damir.denis.tudor.ktor.server.rabbitmq.plugin.RabbitMQ
+import io.github.damir.denis.tudor.ktor.server.rabbitmq.plugin.rabbitmq
 import io.ktor.server.application.*
+import io.ktor.server.request.receiveText
+import io.ktor.server.response.respondText
+import io.ktor.server.routing.application
+import io.ktor.server.routing.get
+import io.ktor.server.routing.routing
 
 fun Application.install() {
-    install(KabbitMQ) {
-        uri = "amqp://guest:guest@localhost:5672"
-    }
+    install(RabbitMQ)
 
-    queueBind {
-        queue = "dlq"
-        exchange = "dlx"
-        routingKey = "dlq-dlx"
-        queueDeclare {
+    rabbitmq {
+        queueBind {
             queue = "dlq"
-            durable = true
-        }
-        exchangeDeclare {
             exchange = "dlx"
-            type = BuiltinExchangeType.DIRECT
+            routingKey = "dlq-dlx"
+            exchangeDeclare {
+                exchange = "dlx"
+                type = "direct"
+            }
+            queueDeclare {
+                queue = "dlq"
+                durable = true
+            }
+        }
+        queueBind {
+            queue = "test-queue"
+            exchange = "test-exchange"
+            routingKey = "test-routing-key"
+            exchangeDeclare {
+                exchange = "test-exchange"
+                type = "direct"
+            }
+            queueDeclare {
+                queue = "test-queue"
+                arguments = mapOf(
+                    "x-dead-letter-exchange" to "dlx",
+                    "x-dead-letter-routing-key" to "dlq-dlx"
+                )
+            }
         }
     }
 
-    queueBind {
-        queue = "test-queue"
-        exchange = "test-exchange"
-        routingKey = "test-routing-key"
-        queueDeclare {
-            queue = "test-queue"
-            arguments = mapOf(
-                "x-dead-letter-exchange" to "dlx",
-                "x-dead-letter-routing-key" to "dlq-dlx"
-            )
+    routing {
+        get("/rabbitmq") {
+            rabbitmq {
+                basicPublish {
+                    exchange = "demo-exchange"
+                    routingKey = "demo-routing-key"
+                    message { "Hello Ktor!" }
+                }
+            }
+            call.respondText("Hello RabbitMQ!")
         }
-        exchangeDeclare {
-            exchange = "test-exchange"
-            type = BuiltinExchangeType.FANOUT
+
+
+        rabbitmq {
+            basicConsume {
+                autoAck = true
+                queue = "demo-queue"
+                deliverCallback<String> { tag, message ->
+                    log.debug("Received message: $message")
+                }
+            }
         }
     }
 }
