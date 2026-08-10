5fee7ba893aa76cc60e6b51c924fb92988159ace
Bruce Hamilton <bruce.hamilton@jetbrains.com>
2026-08-10 09:12:16 +0200

    Update Ktor to 3.5.2

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -44,7 +44,7 @@ dependencyResolutionManagement {
     }
     versionCatalogs {
         create("ktorLibs") {
-            from("io.ktor:ktor-version-catalog:3.5.1")
+            from("io.ktor:ktor-version-catalog:3.5.2")
         }
     }
 }
