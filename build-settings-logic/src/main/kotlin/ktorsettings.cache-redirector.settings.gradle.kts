0f66686888866ec366bb3df2d74e5761729de957
Osip Fatkullin <osip.fatkullin@jetbrains.com>
2026-09-18 18:06:49 +0200

    Remove redirects from build-settings plugin in favor of init-script

--- build-settings-logic/src/main/kotlin/ktorsettings.cache-redirector.settings.gradle.kts
+++ build-settings-logic/src/main/kotlin/ktorsettings.cache-redirector.settings.gradle.kts
@@ -4,87 +4,30 @@
 
 import java.net.URI
 
-// Copied from the Kotlin project
-// Source: https://github.com/JetBrains/kotlin/blob/v2.3.10/repo/gradle-settings-conventions/cache-redirector/src/main/kotlin/cache-redirector.settings.gradle.kts
-
 private val ProviderFactory.isCIRun: Provider<Boolean>
     get() = gradleProperty("teamcity").map { true }
         .orElse(environmentVariable("TEAMCITY_VERSION").map { true })
         .orElse(false)
 
-/**
- * Enable cache redirector if explicitly set via project property,
- * or when running in CI environment.
- */
-private val Settings.useCacheRedirector: Provider<Boolean>
-    get() = providers
-        .gradleProperty("ktorbuild.useCacheRedirector")
-        .map { it.toBoolean() }
-        .orElse(providers.isCIRun)
-
-// Repository override section
-
-/**
- *  The list of repositories supported by cache redirector should be synced with the "Table of redirects" at https://cache-redirector.jetbrains.com
- *  To add a repository to the list, create an issue in the ADM project (example issue https://youtrack.jetbrains.com/issue/IJI-149)
- *  Or send a merge request to https://jetbrains.team/p/iji/repositories/Cache-Redirector/files/64b69490c54a2a900bb3dd21471f942270289a12/images/config-gen/src/main/kotlin/Config.kt
- */
-val cacheMap: Map<String, String> = mapOf(
-    "https://repo.maven.apache.org/maven2" to "https://cache-redirector.jetbrains.com/repo.maven.apache.org/maven2",
-    "https://plugins.gradle.org/m2" to "https://cache-redirector.jetbrains.com/plugins.gradle.org/m2",
-    "https://dl.google.com/dl/android/maven2" to "https://cache-redirector.jetbrains.com/dl.google.com/dl/android/maven2",
-    "https://redirector.kotlinlang.org/maven/ktor-eap" to "https://cache-redirector.jetbrains.com/redirector.kotlinlang.org/maven/ktor-eap",
-    "https://redirector.kotlinlang.org/maven/dev" to "https://cache-redirector.jetbrains.com/redirector.kotlinlang.org/maven/dev",
-    "https://packages.confluent.io/maven" to "https://cache-redirector.jetbrains.com/packages.confluent.io/maven",
-    "https://jitpack.io" to "https://cache-redirector.jetbrains.com/jitpack.io",
-    "https://services.gradle.org/distributions" to "https://cache-redirector.jetbrains.com/services.gradle.org/distributions",
-    "https://registry.yarnpkg.com" to "https://cache-redirector.jetbrains.com/registry.yarnpkg.com",
-    "https://nodejs.org/dist" to "https://cache-redirector.jetbrains.com/nodejs.org/dist",
-)
-
-val aliases = mapOf(
-    "https://repo1.maven.org/maven2" to "https://repo.maven.apache.org/maven2",
-    "https://maven.google.com" to "https://dl.google.com/dl/android/maven2",
-)
-
-fun String.maybeRedirect(): String {
-    val url = this.trimEnd('/')
-    val deAliasedUrl = aliases.getOrDefault(url, url)
-    val cacheUrlEntry = cacheMap.entries.find { (origin, _) -> deAliasedUrl.startsWith(origin) } ?: return this
-
-    val cacheUrl = cacheUrlEntry.value
-    val originRestPath = deAliasedUrl.substringAfter(cacheUrlEntry.key, "")
-    return "$cacheUrl$originRestPath"
-}
-
-fun URI.maybeRedirect(): URI = URI(toString().maybeRedirect())
-
-fun RepositoryHandler.redirect() = configureEach {
-    when (this) {
-        is MavenArtifactRepository -> url = url.maybeRedirect()
-        is IvyArtifactRepository -> @Suppress("SENSELESS_COMPARISON") if (url != null) {
-            url = url.maybeRedirect()
-        }
-    }
-}
+// This extra property is set by `cache-redirector.init.gradle.kts`,
+// so adding that init-script means enabling cache redirector.
+private val Settings.isCacheRedirectorEnabled: Boolean
+    get() = extra.has("ktorbuild.cacheRedirectorEnabled") && extra["ktorbuild.cacheRedirectorEnabled"] == true
 
 fun Project.overrideGradleDistributionUrl() {
     gradle.taskGraph.whenReady {
         tasks.named<Wrapper>("wrapper") {
-            distributionUrl = distributionUrl.maybeRedirect()
+            distributionUrl = distributionUrl.replace(
+                "https://services.gradle.org/distributions",
+                "https://cache-redirector.jetbrains.com/services.gradle.org/distributions",
+            )
         }
     }
 }
 
-// Native compiler download url override section
-
-fun Project.overrideNativeCompilerDownloadUrl() {
-    logger.info("Redirecting Kotlin/Native compiler download url")
-    extensions.extraProperties["kotlin.native.distribution.baseDownloadUrl"] =
-        "https://cache-redirector.jetbrains.com/download.jetbrains.com/kotlin/native/builds"
-}
-
 // Check repositories are overridden section
+// Copied from the Kotlin project
+// Source: https://github.com/JetBrains/kotlin/blob/v2.3.10/repo/gradle-settings-conventions/cache-redirector/src/main/kotlin/cache-redirector.settings.gradle.kts
 abstract class CheckRepositoriesTask : DefaultTask() {
     @get:Input
     val teamcityBuild = project.providers.isCIRun
@@ -210,9 +153,9 @@ fun Project.addCheckRepositoriesTask() {
     tasks.register("checkRepositories", CheckRepositoriesTask::class.java)
 }
 
-fun Project.configureTestTasks(useCacheRedirector: Provider<Boolean>) {
+fun Project.configureTestTasks() {
     tasks.withType<Test>().configureEach {
-        systemProperty("ktorbuild.useCacheRedirector", useCacheRedirector.get())
+        systemProperty("ktorbuild.cacheRedirectorEnabled", true)
         systemProperty(
             "ktorbuild.cacheRedirectorInitScript",
             settingsDir.resolve("gradle/cache-redirector.init.gradle.kts").absolutePath,
@@ -221,20 +164,13 @@ fun Project.configureTestTasks(useCacheRedirector: Provider<Boolean>) {
 }
 
 // Main configuration
+// Repositories are overridden by applying `--init-script gradle/cache-redirector.init.gradle.kts`.
+// Here we apply additional project configurations.
 
-if (useCacheRedirector.get()) {
-    logger.info("Redirecting repositories for settings in ${settingsDir.absolutePath}")
-
-    pluginManagement.repositories.redirect()
-    dependencyResolutionManagement.repositories.redirect()
-    buildscript.repositories.redirect()
-
+if (isCacheRedirectorEnabled) {
     gradle.beforeProject {
-        buildscript.repositories.redirect()
-        repositories.redirect()
-        overrideNativeCompilerDownloadUrl()
         addCheckRepositoriesTask()
-        configureTestTasks(useCacheRedirector)
+        configureTestTasks()
     }
 }
 
